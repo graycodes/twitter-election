3aa3662e84294c44f02203c1f022b8de38dd6561
Gray MacGregor <gmacg.uk@gmail.com>
2015-04-29 07:57:33 +0100

    Added uk bounding box

--- index.js
+++ index.js
@@ -12,23 +12,58 @@ fs.readFile('./secret.json', 'utf8', function (err,data) {
     var parties = ['labour', 'conservative', 'tory', 'tories', 'lib dem', 'greens',
                    'green party', 'tusc', 'snp', 'plaid', 'ukip'];
 
+    var partyNames = [ 'Labour', 'Conservative', 'SNP', 'Lib Dems', 'Green', 'TUSC', 'Plaid', 'UKIP' ];
+
+    var partyScores = _.map(partyNames, function (p) {
+        var o = {};
+        return o[p] = 0;
+    });
+    console.log(partyScores);
+
     var stream = T.stream('statuses/filter', {
         track: parties.join(','),
-        language: 'en'/*,
-        locations: ['-13.44', '62.50', '2.10', '50.15']*/
+        locations: ['-10.93', '49.83', '1.43', '62.86']
     });
 
     stream.on('tweet', function (tweet) {
         var tags = _.pluck(tweet.entities.hashtags, 'text');
         var matcher = new RegExp('(' + parties.join('|') + ')','gi');
         var party = tweet.text.match(matcher);
-        if (party) {
-            console.log(_.uniq(_.map(party, function (p) {
-                return p.toLowerCase();
-            })));
-        }
-        console.log('');
+        var partiesTweeted;
+
+        if (!party) return;
+
+        partiesTweeted = (_.uniq(_.map(party, function (p) {
+            return p.toLowerCase();
+        })));
+
+        console.log(partiesTweeted);
 
     });
+
+    //TODO finish this.
+    function storeParties(parties) {//todo fix multiple insert to one party via 2 names
+        while (parties.length) {
+            var p = parties.pop();
+            switch (p) {
+                case 'labour':
+                    partyScores['Labour']++;
+                    break;
+                case 'conservative':
+                case 'tory':
+                case 'tories':
+                    partyScores['Conservative']++;
+                    break;
+                case 'lib dem':
+                    partyScores['Lib Dems']++;//?
+                    break;
+                case 'greens':
+                case 'green':
+                    partyScores['Green']++;
+                
+            }
+        }
+    }
+
     return undefined;
 });
